ac6ed6f95932ab5146e5bd9741aea7e5b72bfadd
Mukhammadali Khayotov <31897554+MuhammadAliHayotov@users.noreply.github.com>
2022-06-13 13:26:11 +0500

    Project Adding (design and sources downloaded from appbrewery)

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    var weatherManager = WeatherManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self //textfield should report back on what's happening
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        
+    }
+    //this one is for the return button on the keyboard so that user does not have to press the IBButton all the time
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    // to check if the user has actually typed something and then pressed go; if not then prompt the user to type something and only after that go to the next process
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Type something here"
+            return false
+        }
+    }
+    //so that when user inputs sth and presses search button it searches for that thing and clears out the textfield
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //if the city is a 
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        searchTextField.text = ""
+    }
+    
+}
+
---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by A253 on 13/06/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=08d047857b2e1e073f3ca56ced948860&units=metric"
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+    }
+}
