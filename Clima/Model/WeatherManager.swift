4e26abe8c54d1d6308920006e3d888b75bbd7e13
Mukhammadali Khayotov <31897554+MuhammadAliHayotov@users.noreply.github.com>
2022-06-13 14:08:11 +0500

    Setting up URL Session

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -12,7 +12,40 @@ struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=08d047857b2e1e073f3ca56ced948860&units=metric"
     
     func fetchWeather(cityName: String){
-        let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        let urlString = "\(weatherURL)&q=\(cityName)"//prepare the url (fetch the key)
+        performRequest(urlString: urlString)//pass it to a performer
+    }
+    func performRequest(urlString: String){
+        //TODO:
+        /*
+         1. Create a URL
+         2. Create a URLSession
+         3. Give the session a task
+         4. Start the Task
+         */
+        
+        //Creating a URL
+        if let url = URL(string: urlString){
+            //Creatinga URL Session
+            let session = URLSession(configuration: .default)
+            //giving the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            //starting the task
+            task.resume()
+        }
+    }
+    
+    func handle(data:Data?, response:URLResponse?, error:Error?){
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data:safeData, encoding: .utf8)//we are getting data as a string
+            print(dataString as Any)
+            
+        }
     }
 }
